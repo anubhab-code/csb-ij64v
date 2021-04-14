d1f2f6652333becf47a53528328ecef2e3eeec4b
Code Sandbox <hello@codesandbox.io>
2021-04-14 10:55:11 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.1b48cc2444b7f7bf0d7a04db1793fda7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22ee384a94c7889ee2ecea3549e993ab",
+    "url": "https://anubhab-code.github.io/csb-ij64v/index.html"
+  },
+  {
+    "revision": "f81edc913172b25674cd",
+    "url": "https://anubhab-code.github.io/csb-ij64v/static/css/main.77f54260.chunk.css"
+  },
+  {
+    "revision": "a53e24498f065a27ea6c",
+    "url": "https://anubhab-code.github.io/csb-ij64v/static/js/2.87bd56a9.chunk.js"
+  },
+  {
+    "revision": "1f856904e7e7ca93e4849cc435183cdc",
+    "url": "https://anubhab-code.github.io/csb-ij64v/static/js/2.87bd56a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f81edc913172b25674cd",
+    "url": "https://anubhab-code.github.io/csb-ij64v/static/js/main.09c78922.chunk.js"
+  },
+  {
+    "revision": "b25601bc0584fdbaf17e",
+    "url": "https://anubhab-code.github.io/csb-ij64v/static/js/runtime-main.912765c2.js"
+  },
+  {
+    "revision": "9432ddaf699e4df5f361bd94dec8925c",
+    "url": "https://anubhab-code.github.io/csb-ij64v/static/media/first_graphic.9432ddaf.svg"
+  },
+  {
+    "revision": "71bb19a1a747b0e3818eae2f0a9428f5",
+    "url": "https://anubhab-code.github.io/csb-ij64v/static/media/profileAvatar.71bb19a1.jpg"
+  },
+  {
+    "revision": "6b55a6169ceac7ef3448475ccbf41d6e",
+    "url": "https://anubhab-code.github.io/csb-ij64v/static/media/second_graphic.6b55a616.svg"
+  }
+]);
